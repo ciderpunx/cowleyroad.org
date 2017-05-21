8023086c76328e74f46896a8de1cf017b68f56cc
Charlie Harvey <charlie@mallory.hendred.net>
2017-05-21 14:00:47 +0100

    Change bg colour of unloaded slides to white

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ html,body {font-family: Montserrat, sans-serif;text-align:center;margin:0}
 #front-logobar p {padding:0 0 3em 0;margin-top:-1em}
 #front-article {width:100%; overflow-x:hidden; overflow-y:hidden}
 .swiper-container {width:95%;height:350px}
-.swiper-slide {width:250px;height:350px;background-color:#ccc;overflow:hidden}
+.swiper-slide {width:250px;height:350px;background-color:#fff;overflow:hidden}
 .swiper-slide a {line-height:350px;text-decoration:none; color:#999;}
 .swiper-slide img {height:350px;object-position:center;object-fit:contain}
 .swiper-button-next, .swiper-button-prev {
@@ -107,13 +107,9 @@ html,body {font-family: Montserrat, sans-serif;text-align:center;margin:0}
 }
 .swiper-button-next, .swiper-container-rtl .swiper-button-prev {
   background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M27%2C22L27%2C22L5%2C44l-2.1-2.1L22.8%2C22L2.9%2C2.1L5%2C0L27%2C22L27%2C22z'%20fill%3D'%23000000'%2F%3E%3C%2Fsvg%3E");
-  right: 10px;
-  left: auto;
 }
 .swiper-button-prev, .swiper-container-rtl .swiper-button-next {
   background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M0%2C22L22%2C0l2.1%2C2.1L4.2%2C22l19.9%2C19.9L22%2C44L0%2C22L0%2C22L0%2C22z'%20fill%3D'%23000000'%2F%3E%3C%2Fsvg%3E");
-  left: 10px;
-  right: auto;
 }
 .swiper-button-disabled {display:none}
 <!--[if (lte IE 10)|!(IE)]><!-->
@@ -135,7 +131,8 @@ function lazyLoadImages() {
   [].forEach.call(images, function (item) {
     if (isElementInViewport(item)) {
       item.setAttribute("src",item.getAttribute("data-src"));
-      item.removeAttribute("data-src")
+      item.removeAttribute("data-src");
+      // TODO: change parent bgcolor to white on load
     }
   })
   // if all the images are loaded, stop calling the handler
