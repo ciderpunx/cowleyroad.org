47bceefc7d283da87fc04e373f61f0eba0d9bd40
Charlie Harvey <charlie@mallory.hendred.net>
2017-05-21 13:59:59 +0100

    Slider arrow styling improved

--- index.php
+++ index.php
@@ -100,6 +100,11 @@ html,body {font-family: Montserrat, sans-serif;text-align:center;margin:0}
 .swiper-slide {width:250px;height:350px;background-color:#ccc;overflow:hidden}
 .swiper-slide a {line-height:350px;text-decoration:none; color:#999;}
 .swiper-slide img {height:350px;object-position:center;object-fit:contain}
+.swiper-button-next, .swiper-button-prev {
+  width: 58px;
+  height: 105px;
+  background-color: rgba(255,255,255,0.5);
+}
 .swiper-button-next, .swiper-container-rtl .swiper-button-prev {
   background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%20viewBox%3D'0%200%2027%2044'%3E%3Cpath%20d%3D'M27%2C22L27%2C22L5%2C44l-2.1-2.1L22.8%2C22L2.9%2C2.1L5%2C0L27%2C22L27%2C22z'%20fill%3D'%23000000'%2F%3E%3C%2Fsvg%3E");
   right: 10px;
@@ -110,6 +115,7 @@ html,body {font-family: Montserrat, sans-serif;text-align:center;margin:0}
   left: 10px;
   right: auto;
 }
+.swiper-button-disabled {display:none}
 <!--[if (lte IE 10)|!(IE)]><!-->
 #front-article {overflow-x:scroll}
 <![endif]-->
