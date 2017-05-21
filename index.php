4855a4b03dbedc7b4760a3ce37b97abdc89399ca
Charlie Harvey <charlie@mallory.hendred.net>
2017-05-21 21:47:04 +0100

    Added some extra space on RHS to stop glitch
    With rightmost slide not preloading until scrolled past.

--- index.php
+++ index.php
@@ -150,9 +150,9 @@ function isElementInViewport (el) {
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
-    );
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= 550 + (window.innerWidth || document.documentElement.clientWidth) 
+    ); // note that adding a bit to the RHS pre-buffers 2 images, so they display quicker when scrolling right TODO check perf implications
 }
 </script>
 
